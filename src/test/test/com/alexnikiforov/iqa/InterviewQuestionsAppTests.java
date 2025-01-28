7195e5dbb9dc65361bd68f50890b87ba40e3d9dd
Alexander Nikiforov <nikiforov.java@gmail.com>
2025-01-28 23:50:04 +0400

    Set up spring boot project structure

---
+++ src/test/test/com/alexnikiforov/iqa/InterviewQuestionsAppTests.java
@@ -0,0 +1,13 @@
+package com.alexnikiforov.iqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewQuestionsAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
